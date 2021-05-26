a18456b7266233b4ff7bdeba0904310cec2a2e0e
Li <Mingqiang.Li@cn.bosch.com>
2021-05-26 20:22:11 +0800

    first commit

---
+++ Python/PycharmProjects/Turtle.py
@@ -0,0 +1,13 @@
+import turtle
+from turtle import *
+
+# turtle.setup(300,400,0,0)
+# turtle.screensize(canvwidth=None,canvheight=None,bg=None)
+#
+# turtle.circle(20)
+
+for i in range(10):
+    forward(i)
+    left(90)
+
+turtle.mainloop()
